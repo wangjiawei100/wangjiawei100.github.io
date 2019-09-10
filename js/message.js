e3e41cc82c006a6995535f06a040cb7606b4950a
wangjiawei100 <50898555+wangjiawei100@users.noreply.github.com>
2019-09-10 13:53:10 +0800

    Create message.js

---
+++ js/message.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+    var btn = document.getElementById('publish');
+    btn.addEventListener('click', function(){
+        var content = document.getElementById('content').value;
+        console.log(content)
+        if ( content == '') {
+            alert('发布内容不能为空');
+            return false;
+        }
+        var li = document.createElement('li');
+        li.className = 'clearfix';
+        var html = '';
+        html+='    <img class="icon" src="../img/aboutMe.png" alt="">';
+        html+='    <div class="mesBoardBox">';
+        html+='        <h5>神秘游客</h5>';
+        html+='        <p>'+content+'</p>';
+        html+='    </div>';
+        li.innerHTML = html;
+        document.getElementById('msgList').prepend(li);
+    })
+}
